fd3451fca483c852bf5dd912e1aa4a5c69dcb544
Judy Soukkhaphon <judysoukkhaphon@gmail.com>
2023-01-16 13:19:16 -0500

    add values in an array, was supposed to write with the reduce method but didn't

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
+let totalBatteries=0;
 // Code your solution here
+function reducer() {
+    
+    for (let i = 0; i < batteryBatches.length; i++){
+        totalBatteries = totalBatteries + batteryBatches[i];
+    }
+    
+    
+    console.log(totalBatteries);
+    return totalBatteries;
+    
+}
+
+reducer();
